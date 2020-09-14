65375cba7c6addfb9e4c4c7e5a8862ba3fe7e21a
Gardusio <fabioinnabarbieri@gmail.com>
2020-09-14 13:23:13 +0200

    status bar color, android os target

--- StaseraNonEsco/StaseraNonEsco.Android/MainActivity.cs
+++ StaseraNonEsco/StaseraNonEsco.Android/MainActivity.cs
@@ -19,7 +19,6 @@ namespace StaseraNonEsco.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
